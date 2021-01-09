cd310920b47fc241222f2cf8b72015c1669342ff
Vishwanath Acharya <bailorevishwanath02@gmail.com>
2021-01-09 22:03:36 +0530

    Processing CSV file

--- api/main.py
+++ api/main.py
@@ -1,9 +1,12 @@
 from typing import List
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, File, UploadFile, HTTPException
 from pydantic import BaseModel, ValidationError, validator
 import numpy as np
 from .ml.model import get_model, Model, n_features
+import pandas as pd
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+
 
 class PredictRequest(BaseModel):
     data: List[List[float]]
@@ -25,3 +28,25 @@ def predict(input: PredictRequest, model: Model = Depends(get_model)):
     y_pred = model.predict(X)
     result = PredictResponse(data=y_pred.tolist())
     return result
+
+@app.post("/predict_csv")
+def predict_csv(csv_file: UploadFile = File(...), model: Model = Depends(get_model)):
+    try:
+        df = pd.read_csv(csv_file.file).astype(float)
+    except:
+        raise HTTPException(
+                status_code=HTTP_422_UNPROCESSABLE_ENTITY, detail="Unable to process file"
+                )
+
+    df_n_instances, df_n_features = df.shape
+    if df_n_features != n_features:
+        raise HTTPException(
+                status_code=HTTP_422_UNPROCESSABLE_ENTITY,
+                detail=f"Each data point must contain {n_features} features"
+                )
+
+    y_pred = model.predict(df.to_numpy.reshape(-1,n_features))
+    result = PredictResponse(data=y_pred.tolist())
+    return result
+
+
